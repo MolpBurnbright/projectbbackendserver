d886d3de167a71fd7d158e2966a5b2dadf2dd82a
Cañeda <rodney.l.caneda@accenture.com>
2019-04-11 19:56:32 +0800

    add CORS

--- src/main/java/backend/MoodController.java
+++ src/main/java/backend/MoodController.java
@@ -5,6 +5,7 @@ import backend.models.Mood;
 import org.bson.types.ObjectId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,14 +28,17 @@ public class MoodController {
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public Mood getMoodById(@PathVariable("id") ObjectId id) {
 		return repository.findBy_id(id);
-	}
-
+    }
+    
+//    @CrossOrigin(origins = "http://localhost:9000")
+    @CrossOrigin(origins = "*")
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public void modifyMoodById(@PathVariable("id") ObjectId id, @Valid @RequestBody Mood mood) {
 	mood.set_id(id);
 		repository.save(mood);
 	}
 
+    @CrossOrigin(origins = "*")
 	@RequestMapping(value = "/", method = RequestMethod.POST)
 	public Mood createMood(@Valid @RequestBody Mood mood) {
 		mood.set_id(ObjectId.get());
